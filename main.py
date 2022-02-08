b31312012431279113088524762c2ff687782bd6
N44 <nandhu90040@gmail.com>
2022-02-08 18:32:58 +0530

    Added Initial code
    Main and initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# To create table `Employee` to new database company
+import mysql.connector as sqltor
+
+# Reading password!
+passwordFile = open("password.txt","r+")
+passWord = passwordFile.read()
+passwordFile.close()
+
+#Establishing connection
+mydb = sqltor.connect(host='localhost',user='root',password=f'{passWord}')
+
+# Using cursor() to prepare cursor
+mc=mydb.cursor()
+mc.execute('CREATE DATABASE IF NOT EXISTS Company;')
+print("Created database \"Company\". ")
+mc.execute('USE Company;')
+print("Useing database \"Company\". ")
